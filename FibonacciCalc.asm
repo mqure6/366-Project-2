7acc76de5dc5a21a618f8bc4e03e68b953a2bc19
Moiz <mqure6@uic.edu>
2025-04-01 21:44:06 -0500

    Base 1A Code
    Currently "drops off bottom" on line 16 branch with message "Step: execution terminated due to null instruction"
    Things to note:
    1. Apparently you can load immediate values with li like how MOVS does in ARM Assembly, very handy indeed
    2. Labels need colons or they get recognized as invalid operators

--- FibonacciCalc.asm
+++ FibonacciCalc.asm
@@ -1,3 +1,25 @@
 # See Psueodocode in Doc or "fib" Function in 366Project2.c
 
-    .text
+	
+	li $s0, 5	# Insert "n" to s0; supposedly n is in memory "somewhere", 5 is a placeholder
+	li $s1, 0	# s1 will be used as a counter during the for loop needed for the Fib sequence
+	sub $s3, $s0, 1  # s3 = n - 1, used in the condition for the Fib sequence loop
+	
+	# Load the registers used in the Fibonacchi calculation
+	li $t1, 0	# t1 = "a"
+	li $t2, 1	# t2 = "b"
+	
+	ble $s0, 1, baseEnd 	# if(n<=1), jump to end of function
+	
+	# t0 acts as a "temp" variable, t1 holds the final value
+	FibSeq:	
+		blt $s1, $s3, end	# if i (s1) < n - 1 (s3), jump to end 
+		lw $t0, ($t2)
+		add $t2, $t1, $t2
+		lw $t1, ($t0)
+		add $s1, $s1, 1		# increment counter
+		b FibSeq		# loop back to the branch condition
+	
+	baseEnd:	# End function based on base condition, returns s0
+
+	end:	# End function after successful Fib computation, returns t1
