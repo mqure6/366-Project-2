f816e0071293cbf94870ec27129f72884075fa2e
Zaleu <155387541+Zaleu@users.noreply.github.com>
2025-04-02 21:10:29 -0500

    asm changes

--- FibonacciCalc.asm
+++ FibonacciCalc.asm
@@ -1,5 +1,6 @@
 # See Psueodocode in Doc or "fib" Function in 366Project2.c
-
+	.data
+	result: .space 4  # Allocate 4 bytes for the Fibonacci result
 	
 	li $s0, 5	# Insert "n" to s0; supposedly n is in memory "somewhere", 5 is a placeholder
 	li $s1, 0	# s1 will be used as a counter during the for loop needed for the Fib sequence
@@ -24,7 +25,8 @@
 		move $t1, $s0	# End function based on base condition, returns s0
 
 	end:
-    	sw $t1, n      # Store result back to memory
-		# Exit program
-		li $v0, 10     # Exit syscall
-    	syscall
+    la $t3, result    # Load address of result
+    sw $t1, 0($t3)    # Store Fibonacci result in memory
+
+    li $v0, 10        # Exit syscall
+    syscall
