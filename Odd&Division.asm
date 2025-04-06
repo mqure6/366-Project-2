bb1f6ec2f2d551a7176a9750168f29c22242e4b2
Moiz <mqure6@uic.edu>
2025-04-05 20:33:22 -0500

    Complete Odd&Division functions
    This implementation does successfully calculate the parity of a non-negative integer.
    Things to note:
    1. This thing really doesn't like negative numbers for whatever reason, but the Fibonacci sequence doesn't contain negatives, so I won't investigate further.
    2. A move function was needed to transfer the contents of $t0 to $t1. Theoretically this could have been done instead as "addi $t1, $t0, 0", but I prefer the more explicit call, even if it is an extended instruction
    3. Read the note on lines 27-29

--- Odd&Division.asm
+++ Odd&Division.asm
@@ -7,28 +7,32 @@ division:
 	li $t4, 2	# Because we need to do m%2, or m - 2 a lot
 	
 divWhile:
-	ble $t0, $t4, divEnd	# If remainder <= 2, stop
+	blt $t0, $t4, divEnd	# If remainder <= 2, stop
 	sub $t0, $t0, $t4
 	addi $t3, $t3, 1
 	b divWhile	  
 	
 divEnd:
-	lw $t1, ($t0)	# Remainder = result of division
+	move $t1, $t0	# Remainder = result of division
 	b divReturn
 	
 odd:	
-	li $t0, 6	# Placeholder, replace this with getting the value from FibonacciCalc
+	li $t0, 7	# Placeholder, replace this with getting the value from FibonacciCalc
 	li $t1, 0	# t1 = remainder
 	
 	b division	# Go do division
+	
+returnFalse:
+	li $v0, 0	# Return "fibNum is even"
+	# NOTE: This causes an infinite loop as line 27 keeps bringing us back here
+	# In the final implementation there will be somewhere for us to branch off from here
+	# The calculation is still done correctly, but it needs to be manually stopped...
 	
 divReturn:
 	beq $t1, 0, returnFalse	# If remainder = 0, fibNum is even
 	
 	li $v0, 1	# Return "fibNum is odd"
-	
-returnFalse:
-	li $v0, 0	# Return "fibNum is even"
+
 	
 	
 	
