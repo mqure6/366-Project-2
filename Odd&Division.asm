aa5f100c1e25c8cfab7e3fb83d65c240d7483f3a
Moiz <mqure6@uic.edu>
2025-04-05 19:59:37 -0500

    Complete skeleton
    Finish the "This will probably work" skeleton code

--- Odd&Division.asm
+++ Odd&Division.asm
@@ -1,14 +1,20 @@
 # See Psueodocode in Doc or "odd" and "division" functions in 366Project2.c
 
+	b odd	# Start the program at odd
+
 division: 
 	li $t3, 0	# t3 = quotient
 	li $t4, 2	# Because we need to do m%2, or m - 2 a lot
+	
 divWhile:
 	ble $t0, $t4, divEnd	# If remainder <= 2, stop
-	#This function needs to end by branching back to "odd"
+	sub $t0, $t0, $t4
+	addi $t3, $t3, 1
+	b divWhile	  
 	
 divEnd:
-	#go back to wherever we left off in "odd"
+	lw $t1, ($t0)	# Remainder = result of division
+	b divReturn
 	
 odd:	
 	li $t0, 6	# Placeholder, replace this with getting the value from FibonacciCalc
@@ -16,6 +22,7 @@ odd:
 	
 	b division	# Go do division
 	
+divReturn:
 	beq $t1, 0, returnFalse	# If remainder = 0, fibNum is even
 	
 	li $v0, 1	# Return "fibNum is odd"
